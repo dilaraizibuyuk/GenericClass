52c286b651b160a2e6477f06837cdc11cb18f666
Dilara <74327986+dilaraizibuyuk@users.noreply.github.com>
2022-01-14 20:01:28 +0300

    Proje dosyası ekle.

---
+++ GenericClass/GenericClassım.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClass
+{
+    public class MyList<T>
+    {
+        private static int nesneSay = 0;
+        public MyList()
+        {
+            nesneSay++;
+        }
+        public int Say
+        {
+            get
+            {
+                return nesneSay;
+            }
+        }
+    }
+}
---
+++ GenericClass/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClass
+{
+    class Program
+    {
+        class SampleClass
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            MyList<int> myIntList = new MyList<int>();
+            MyList<int> myIntList2 = new MyList<int>();
+
+            MyList<double> myDoubleList = new MyList<double>();
+
+            MyList<SampleClass> mySampleList = new MyList<SampleClass>();
+            Console.WriteLine(myIntList.Say);
+            Console.WriteLine(myIntList2.Say);
+            Console.WriteLine(myDoubleList.Say);
+            Console.WriteLine(mySampleList.Say);
+            Console.WriteLine(new MyList<SampleClass>().Say);
+            Console.ReadLine();
+
+        }
+    }
+}
